74c14b91f45f82d6fed288498e857ceb6fcf1f84
Aleph Retamal <aleph.retamal@airtasker.com>
2018-11-15 18:18:32 +1100

    Add error case to playground

--- Coindar.playground/Pages/SplashViewController.xcplaygroundpage/Contents.swift
+++ Coindar.playground/Pages/SplashViewController.xcplaygroundpage/Contents.swift
@@ -9,7 +9,7 @@ class CancellableMock: Cancellable {
     func cancel() { }
 }
 
-func simulateRequestWithProgress<T>(_ timeInterval: TimeInterval, _ progress: @escaping (Double) -> Void, _ onSuccess: @escaping ([T]) -> Void) -> Cancellable {
+func simulateSuccessRequestWithProgress<T>(_ timeInterval: TimeInterval, _ progress: @escaping (Double) -> Void, _ onSuccess: @escaping ([T]) -> Void) -> Cancellable {
     var prog: Double = 0
 
     var timer: Timer?
@@ -21,7 +21,26 @@ func simulateRequestWithProgress<T>(_ timeInterval: TimeInterval, _ progress: @e
                 onSuccess([])
                 timer?.invalidate()
             } else {
-                print(prog)
+                progress(prog)
+            }
+        }
+    }
+
+    return CancellableMock()
+}
+
+func simulateErrorRequestWithProgress(_ timeInterval: TimeInterval, _ progress: @escaping (Double) -> Void, _ onError: @escaping (Error) -> Void) -> Cancellable {
+    var prog: Double = 0
+
+    var timer: Timer?
+    timer = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: true) { _ in
+        prog += 0.1
+        DispatchQueue.main.async {
+            if prog >= 0.5 {
+                progress(1)
+                onError(MoyaError.requestMapping("Error r r r r"))
+                timer?.invalidate()
+            } else {
                 progress(prog)
             }
         }
@@ -32,11 +51,13 @@ func simulateRequestWithProgress<T>(_ timeInterval: TimeInterval, _ progress: @e
 
 class CoindarMock: Coindar {
     override func getCoins(progress: @escaping (Double) -> Void, onSuccess: @escaping ([Coin]) -> Void, onError: @escaping (Error) -> Void) -> Cancellable {
-        return simulateRequestWithProgress(1, progress, onSuccess)
+//        return simulateSuccessRequestWithProgress(1, progress, onSuccess)
+        return simulateErrorRequestWithProgress(1, progress, onError)
     }
 
     override func getTags(progress: @escaping (Double) -> Void, onSuccess: @escaping ([Tag]) -> Void, onError: @escaping (Error) -> Void) -> Cancellable {
-        return simulateRequestWithProgress(0.5, progress, onSuccess)
+//        return simulateSuccessRequestWithProgress(0.5, progress, onSuccess)
+        return simulateErrorRequestWithProgress(0.5, progress, onError)
     }
 }
 
