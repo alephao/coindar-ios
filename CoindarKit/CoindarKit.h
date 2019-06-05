39a9e008bcab9191adcb507910247ee9a9f04989
Aleph Retamal <retamal.ph@gmail.com>
2019-06-05 20:45:22 +1000

    Create CoindarKit

---
+++ CoindarKit/CoindarKit.h
@@ -0,0 +1,13 @@
+// Copyright © lalacode.io All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoindarKit.
+FOUNDATION_EXPORT double CoindarKitVersionNumber;
+
+//! Project version string for CoindarKit.
+FOUNDATION_EXPORT const unsigned char CoindarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoindarKit/PublicHeader.h>
+
+
