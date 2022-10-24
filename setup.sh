7f85834e2d733cc9458d4c0b3566e8df92eb67dd
Afonso <4775087+afonsograca@users.noreply.github.com>
2022-10-24 22:29:19 +0100

    :sparkles:(setup): add steps to install bundler and pods (#16)

--- setup.sh
+++ setup.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+
+# Step: Bundler
+if ! command -v bundle >/dev/null 2>&1; then
+  echo "Please install bundler before proceeding (run `gem install bundler`)"
+  exit 1
+fi
+bundle install
+
+# Step: Cocoapods
+if ! command -v pod >/dev/null 2>&1; then
+  echo "Please install Cocoapods before proceeding (run `gem install cocoapods`)"
+  exit 1
+fi
+bundle exec pod install
+
+
 cp SampleConfig.swift Config.swift
